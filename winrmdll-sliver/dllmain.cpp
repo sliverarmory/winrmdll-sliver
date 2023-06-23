c80dccfd08d366439c53b9b7934d31a763fb9ff4
c2biz <ccardio@bishopfox.com>
2023-06-23 17:04:16 -0400

    successful remote execution via winrm

--- winrmdll-sliver/dllmain.cpp
+++ winrmdll-sliver/dllmain.cpp
@@ -59,15 +59,12 @@ Arguments::Arguments(const char* argument_string) {
 	std::string new_argstr(argument_string);
 	std::string arg;
 	std::stringstream ss(new_argstr);
-	//append(output, "[Arguments] argument_string size: %u\n", strlen(argument_string));
-	//append(output, "[Arguments] new_argstr size: %u\n", new_argstr.size());
 	std::vector<std::string> args;
 	while (ss >> arg) {
 		args.push_back(arg);
 	}
 
 	this->Hostname = args[0];
-	//this->Hostname = "dest2.vic.local";
 	this->Command = args[1];
 	this->Username = args[2];
 	this->Password = args[3];
